767e67168591a7f8224879f1cf3cb4a196ad1466
Luke Bergmann <lukebergmann0@gmail.com>
2021-04-27 16:06:55 +0000

    Updated store 1 and 2 with instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+puts @store1.name
+@store2 = Store.find(2)
+puts @store2.name
+@store1.update(name: "Nelson", annual_revenue: 131313)
