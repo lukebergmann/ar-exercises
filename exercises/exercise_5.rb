1f6f551dd26d00b84db857466f4b1daf7f5f1e7c
Luke Bergmann <lukebergmann0@gmail.com>
2021-04-27 17:03:34 +0000

    Calculated gross annual_income of all the stores and found stores that made more than 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# ALL stores annual revenue added together
+puts Store.sum(:annual_revenue)
+
+# AVERAGE annual_revenue for ALL stores
+puts Store.average(:annual_revenue)
+
+# ALL stores that generate MORE than 1M
+@high_grossing = Store.where(["annual_revenue > ?", "1000000"])
+puts @high_grossing.count
+@high_grossing.each{ |store| puts "#{store.name}: #{store.annual_revenue}"}
