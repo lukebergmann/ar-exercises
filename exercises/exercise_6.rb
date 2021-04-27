747e19f064e823efc72e9f6b2af5b8d38c47d1c8
Luke Bergmann <lukebergmann0@gmail.com>
2021-04-27 17:18:12 +0000

    created One-to_Many associations by adding in a handful of new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Luke", last_name: "Bergmann", hourly_rate: 65)
+@store1.employees.create(first_name: "Alyssa", last_name: "Schollaardt", hourly_rate: 100)
+@store1.employees.create(first_name: "Elphie", last_name: "Mo", hourly_rate: 88)
+@store1.employees.create(first_name: "Floki", last_name: "Wan", hourly_rate: 43)
+@store2.employees.create(first_name: "Ragnar", last_name: "Lothbrook", hourly_rate: 50)
+@store2.employees.create(first_name: "Bjorn", last_name: "Ironside", hourly_rate: 73)
+@store2.employees.create(first_name: "Ivar", last_name: "TheBoneless", hourly_rate: 60)
+@store2.employees.create(first_name: "Ecgberth", last_name: "KingOfWessex", hourly_rate: 99)
